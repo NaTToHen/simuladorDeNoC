17e0241460abe4a75c76e216c0fb5b2d600baeb0
Henrique Bonatto <nattohen@gmail.com>
2023-10-18 20:34:55 -0300

    primeiro

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    val quantNodos = readln()
+    val source = readln()
+    val target = readln()
+
+    if(source.toInt() < quantNodos.toInt() || target.toInt() < quantNodos.toInt()) {
+            println("esse nodo não existe")
+    } else {
+        println()
+        (1..quantNodos.toInt()).forEach { i ->
+
+        }
+    }
+
+
+}
