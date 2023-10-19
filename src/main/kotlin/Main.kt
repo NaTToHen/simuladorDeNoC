8a92d6c9de98fcf9273915ad4bbf8537cff41ab0
Henrique Bonatto <nattohen@gmail.com>
2023-10-18 21:59:16 -0300

    finaulizado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,61 @@
+import kotlin.math.sign
+
+val quantNodos = readln().toInt()
+
 fun main(args: Array<String>) {
-    val quantNodos = readln()
-    val source = readln()
-    val target = readln()
+    val condicao: Int = 1
+    if (quantNodos <= 10) {
+        while (condicao == 1) {
+            print("Qual é o source: ")
+            var source = readln().toInt()
+            print("Qual é o target: ")
+            var target = readln().toInt()
 
-    if(source.toInt() < quantNodos.toInt() || target.toInt() < quantNodos.toInt()) {
-            println("esse nodo não existe")
-    } else {
-        println()
-        (1..quantNodos.toInt()).forEach { i ->
-
+            arbitragem(source, target)
         }
+    } else {
+        println("O numero maximo de nodos é 10")
     }
+}
 
+fun arbitragem(source: Int, target: Int) {
+    if(source > quantNodos || target > quantNodos || target == source) {
+        println("esse nodo não existe")
+    } else {
+        roteamento(source, target)
+    }
+}
 
+fun roteamento(source: Int, target: Int) {
+    var source = source
+    var target = target
+    if(source == 1 && target == quantNodos || source == quantNodos && target == 1) {
+        println("Proc[${source}] criou a mensagem")
+        println("Proc[${source}] enviou a mensagem para o Proc[${target}]")
+        println("Proc[${target}] recebeu a mensagem do Proc[${source}]")
+        println("Proc[${target}] é o destino e consumiu a mensagem")
+    } else if(source < target) {
+        println("Proc[${source}] criou a mensagem")
+        while (source < target) {
+            println("Proc[${source}] enviou a mensagem para o Proc[${source+1}]")
+            source+=1
+            println("Proc[${source}] recebeu a mensagem de Proc[${source-1}]")
+            if(source < target) {
+                println("Proc[${source}] NÃO é o destino")
+            }
+        }
+        println("Proc[${target}] é o destino\n" + "Proc[${target}] consumiu a mensagem")
+
+    } else if(source > target) {
+        println("Proc[${source}] criou a mensagem")
+        while (source > target) {
+            println("Proc[${source}] enviou a mensagem para o Proc[${source-1}]")
+            source-=1
+            println("Proc[${source}] recebeu a mensagem de Proc[${source+1}]")
+            if(source > target) {
+                println("Proc[${source}] NÃO é o destino")
+            }
+        }
+        println("Proc[${target}] é o destino\n" + "Proc[${target}] consumiu a mensagem")
+        }
 }
