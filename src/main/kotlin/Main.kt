ddae3cf24c63251721f87c26f04af75fa55503f1
Henrique Bonatto <nattohen@gmail.com>
2023-11-22 15:08:45 -0300

    atualização do mash-2d e MPSoC

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,3 @@
-import kotlin.math.sign
-import kotlin.random.Random
-
 fun main(args: Array<String>) {
 
     println("escolha a topologia de rede:")
@@ -45,20 +42,6 @@ fun Ring() {
     }
 }
 
-fun Mash2D() {
-    print("Informe o tamanho da NoC para X: ")
-    var tamanhoX = readln().toInt()
-    print("Informe o tamanho da NoC para Y: ")
-    var tamanhoY = readln().toInt()
-    println("\nConstrução de pacotes:")
-
-    if(tamanhoX <= 9 && tamanhoY <= 9) {
-        acoesMash2D(tamanhoX, tamanhoY)
-    } else {
-        println("O tamanho de X e Y precisam ser menores que 10")
-    }
-}
-
 fun arbitragem(source: Int, target: Int, quantNodos: Int) {
     if(source > quantNodos || target > quantNodos) {
         println("esse nodo não existe")
@@ -79,7 +62,7 @@ fun roteamento(source: Int, target: Int, quantNodos: Int) {
         println("Proc[${source}] criou a mensagem")
         while (source < target) {
             println("Proc[${source}] enviou a mensagem para o Proc[${source+1}]")
-            source+=1
+            source += 1
             println("Proc[${source}] recebeu a mensagem de Proc[${source-1}]")
             if(source < target) {
                 println("Proc[${source}] NÃO é o destino")
@@ -91,7 +74,7 @@ fun roteamento(source: Int, target: Int, quantNodos: Int) {
         println("Proc[${source}] criou a mensagem")
         while (source > target) {
             println("Proc[${source}] enviou a mensagem para o Proc[${source-1}]")
-            source-=1
+            source -= 1
             println("Proc[${source}] recebeu a mensagem de Proc[${source+1}]")
             if(source > target) {
                 println("Proc[${source}] NÃO é o destino")
@@ -101,7 +84,7 @@ fun roteamento(source: Int, target: Int, quantNodos: Int) {
         }
 }
 
-fun acoesMash2D(tamanhoX: Int, tamanhoY: Int) {
+/*fun acoesMash2D(tamanhoX: Int, tamanhoY: Int) {
     print("informar origemX: ")
     val origemX = readln().toInt()
     print("informar origemY: ")
@@ -119,43 +102,101 @@ fun acoesMash2D(tamanhoX: Int, tamanhoY: Int) {
     }
 
     var matrizNodos = Array(tamanhoMatriz) { Array(tamanhoMatriz) { Int } }
+}*/
+
+fun Mash2D() {
+    print("Informe o tamanho da NoC para X: ")
+    var tamanhoX = readln().toInt()
+    print("Informe o tamanho da NoC para Y: ")
+    var tamanhoY = readln().toInt()
+    println("\nConstrução de pacotes:")
+
+    if(tamanhoX <= 9 && tamanhoY <= 9) {
+        acoesMesh2D(tamanhoX, tamanhoY)
+    } else {
+        println("O tamanho de X e Y precisam ser menores que 10")
+    }
+}
+
+fun acoesMesh2D(tamanhoX: Int, tamanhoY: Int) {
+    val matrizNodos = Array(tamanhoY) { Array(tamanhoX) { "" } }
+
+    for (i in 1 until tamanhoY+1) {
+        for (j in 1 until tamanhoX+1) {
+            matrizNodos[i][j] = "${i}${j}"
+        }
+    }
+
+    print("informar origemX: ")
+    val origemX = readln().toInt()
+    print("informar origemY: ")
+    val origemY = readln().toInt()
+
+    print("\ninformar destinoX: ")
+    val destinoX = readln().toInt()
+    print("informar destinoY: ")
+    val destinoY = readln().toInt()
+
+    if (origemX < tamanhoX && origemY < tamanhoY && destinoX < tamanhoX && destinoY < tamanhoY) {
+        roteamentoMesh2D(origemX, origemY, destinoX, destinoY, matrizNodos)
+    } else {
+        println("Origem e destino devem estar dentro dos limites")
+    }
+}
+
+fun roteamentoMesh2D(origemX: Int, origemY: Int, destinoX: Int, destinoY: Int, matrizNodos: Array<Array<String>>) {
+    println("Iniciando roteamento da origem (${origemX}, ${origemY}) para o destino (${destinoX}, ${destinoY}):")
+
+    var posicaoX = origemX
+    var posicaoY = origemY
+
+    while (posicaoX != destinoX || posicaoY != destinoY) {
+        if (posicaoX < destinoX) {
+            posicaoX++
+        } else if (posicaoX > destinoX) {
+            posicaoX--
+        }
+
+        if (posicaoY < destinoY) {
+            posicaoY++
+        } else if (posicaoY > destinoY) {
+            posicaoY--
+        }
+
+        println("Encaminhando para o nó (${posicaoX}, ${posicaoY})")
+    }
+
+    println("Chegou ao destino (${destinoX}, ${destinoY})")
 }
 
 fun MPSoC() {
-    var matrizNodos = arrayOf(
-        arrayOf("03", "13", "23", "33"),
-        arrayOf("02", "12", "22", "32"),
-        arrayOf("01", "11", "21", "31"),
-        arrayOf("00", "10", "20", "30")
-    )
+    val matrizNodos = Array(9) { Array(9) { "" } }
 
-    println("escolha a aplicação: 1, 2 ou 3")
-    print("resposta: ")
-    val aplicacao = readln().toInt()
-
-    fun app1() {
-        var indexX = 1
-        var indexY = 0
-
-        val inicio = matrizNodos[indexX][indexY]
-        var destino = matrizNodos[indexX+1][indexY]
-
-        println("[P] [INICIANDO COMUNICAÇÃO] [${inicio}] [${destino}]")
-        println("[R] [ARBITRAGEM] [${inicio}] [${destino}]")
-
-        println()
-    }
-
-    when(aplicacao) {
-        1 -> {
-            app1()
-        }
-        2 -> {
-           // app2()
-        }
-        3 -> {
-           // app3()
+    for (i in 0 until matrizNodos.size) {
+        for (j in 0 until matrizNodos[i].size) {
+            matrizNodos[i][j] = "${i}${j}" // Preencha os elementos com identificadores únicos
         }
     }
 
+    println("Escolha a aplicação: 1, 2 ou 3")
+    print("Resposta: ")
+    val aplicacao = readLine()?.toIntOrNull() ?: return
+
+    when (aplicacao) {
+        1 -> app1(matrizNodos)
+        //2 -> app2(matrizNodos)
+        //3 -> app3(matrizNodos)
+        else -> println("Escolha de aplicação inválida.")
+    }
+}
+
+fun app1(matrizNodos: Array<Array<String>>) {
+    var indexX = 1
+    var indexY = 0
+
+    val inicio = matrizNodos[indexX][indexY]
+    val destino = matrizNodos[indexX + 1][indexY]
+
+    println("[P] [INICIANDO COMUNICAÇÃO] [$inicio] [$destino]")
+    println("[R] [ARBITRAGEM] [$inicio] [$destino]")
 }
